145e3b81b56a0efee0302ca447df9849d80a996d
Nathesky <nathesmartins@gmail.com>
2024-03-26 22:53:45 -0300

    26/03

--- components/pagesApp/CadastrarDiario.js
+++ components/pagesApp/CadastrarDiario.js
@@ -1,14 +1,75 @@
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Alert
+} from 'react-native';
+
+import firebaseConfig from '../../firebaseConfig';
+
+export default function CadDiario({ navigation }) {
+  const [titulo, setTitulo] = useState(null);
+  const [texto, setTexto] = useState(null);
+  const [data, setData] = useState(null);
+  const [local, setLocal] = useState(null);
+
+  function addDiario() {
+    firebaseConfig.collection('diario').add({
+        titulo: titulo,
+        texto: texto,
+        data: data,
+        local: local,
+    });
+    setTitulo({titulo: ''} );
+    setTexto({texto: ''});
+    setData({data: ''});
+    setLocal({local: ''});
+    Alert.alert("Cadastro", "Diário adicionado com sucesso");
+    navigation.navigate("Home");
+}
 
 
-export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.paragraph}>
-        Cadastrar DIARIO
-      </Text>
-
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.title}>Cadastre suas Entradas</Text>
+      </View>
+      <TextInput
+        autoCapitalize="sentences"
+        style={styles.input}
+        placeholder="Título"
+        onChangeText={setTitulo}
+        value={titulo}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Texto"
+        onChangeText={setTexto}
+        value={texto}
+        multiline={true}
+        numberOfLines={4}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Data"
+        onChangeText={setData}
+        value={data}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Local"
+        onChangeText={setLocal}
+        value={local}
+      />
+      <TouchableOpacity
+        style={styles.btnEnviar}
+        onPress={() => addDiario()}>
+        <Text style={styles.btnTextEnviar}>Enviar</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
@@ -16,13 +77,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    alignItems: 'center',
+    backgroundColor: '#F6F6F6',
   },
-  paragraph: {
-    margin: 24,
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+    color: '#3E4E5E',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    marginBottom: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    backgroundColor: '#fff',
+  },
+  btnEnviar: {
+    backgroundColor: '#F15A29',
+    width: '80%',
+    height: 40,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnTextEnviar: {
+    color: '#fff',
     fontSize: 18,
     fontWeight: 'bold',
-    textAlign: 'center',
   },
 });
--- components/pagesApp/Home.js
+++ components/pagesApp/Home.js
@@ -1,39 +1,46 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { getAuth, onAuthStateChanged, signOut } from '@firebase/auth';
 import firebaseConfig from '../../firebaseConfig';
+import { FlatList } from 'react-native-gesture-handler';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function Home({ navigation }) {
-  
-    const [diario, setDiario] = useState([]);
+  const [diario, setDiario] = useState([]);
 
-  function deleteDiario(id){
-    firebaseConfig.collection("diario").doc(id).delete();
-    Alert.alert("O diário foi deletado com sucesso");
-  }
-
-  useEffect(() =>{
-      firebaseConfig.collection("diario").onSnapshot((query) =>{
-        const list = [];
-        query.forEach((doc) => {
-          list.push({...doc.data(),id: doc.id});
-        });
-        setDiario(list);
+  useEffect(() => {
+    const unsubscribe = firebaseConfig.collection("diario").onSnapshot((querySnapshot) => {
+      const list = [];
+      querySnapshot.forEach((doc) => {
+        list.push({ ...doc.data(), id: doc.id });
       });
-  },[]);
-  
+      setDiario(list);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   const auth = getAuth();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (!user) {
-        navigation.navigate('Login'); 
+        navigation.navigate('Login');
       }
     });
 
-    return () => unsubscribe(); 
+    return () => unsubscribe();
   }, [auth, navigation]);
+
+  const deleteDiario = async (id) => {
+    try {
+      await firebaseConfig.collection("diario").doc(id).delete();
+      Alert.alert("O diário foi deletado com sucesso");
+    } catch (error) {
+      console.error("Erro ao deletar o diário:", error);
+    }
+  };
 
   const fazerLogout = async () => {
     try {
@@ -49,11 +56,43 @@ export default function Home({ navigation }) {
         <Ionicons name="log-out-outline" size={24} color="black" />
       </TouchableOpacity>
 
-    
-
       <View style={styles.content}>
-        <Text style={styles.title}>My Daily</Text>
-        <Text style={styles.subtitle}>Data: 25/03/2024</Text>
+        <Text style={styles.title}>Lista de Dias</Text>
+        <FlatList
+          data={diario}
+          renderItem={({ item }) => (
+            <View style={styles.diarioItem}>
+              <TouchableOpacity
+                onPress={() =>
+                  navigation.navigate("AlterarDiario", {
+                    id: item.id,
+                    titulo: item.titulo,
+                    texto: item.texto,
+                    local: item.local,
+                    data: item.data
+                  })
+                }
+              >
+                <View style={styles.itemContent}>
+                  <Text style={styles.itemTitle}>Título: <Text style={styles.itemText}>{item.titulo}</Text></Text>
+                  <Text style={styles.itemTitle}>Texto: <Text style={styles.itemText}>{item.texto}</Text></Text>
+                  <Text style={styles.itemTitle}>Local: <Text style={styles.itemText}>{item.local}</Text></Text>
+                  <Text style={styles.itemTitle}>Data: <Text style={styles.itemText}>{item.data}</Text></Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => deleteDiario(item.id)} style={styles.deleteButton}>
+                <MaterialCommunityIcons name="delete-empty" size={24} color="red" />
+              </TouchableOpacity>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+        <TouchableOpacity
+          style={styles.addButton}
+          onPress={() => navigation.navigate("Cadastrar Diario")}
+        >
+          <MaterialCommunityIcons name="plus-circle-outline" size={70} color="green" />
+        </TouchableOpacity>
         <Text style={styles.word}>Frase do dia: Vencer e Vencer!</Text>
       </View>
     </View>
@@ -64,10 +103,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F6F6F6',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   content: {
-    flex: 0.6,
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#FFFFFF',
@@ -88,15 +127,35 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     color: '#3E4E5E',
   },
-  subtitle: {
-    fontSize: 16,
-    color: '#3E4E5E',
-    marginBottom: 5,
+  diarioItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
   },
-  word: {
-    fontSize: 20,
+  itemContent: {
+    flex: 1,
+  },
+  itemTitle: {
     fontWeight: 'bold',
-    color: '#F15A29',
+    marginBottom: 5,
+    color: '#3E4E5E',
+  },
+  itemText: {
+    color: '#000',
+  },
+  deleteButton: {
+    marginLeft: 10,
+  },
+  addButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 35,
+    padding: 10,
+    zIndex: 1,
   },
   logoutButton: {
     position: 'absolute',
@@ -106,5 +165,11 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     padding: 10,
     zIndex: 1,
+  },
+  word: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#F15A29',
+    marginTop: 20,
   },
 });
